ce6f4d50fc1739aa254d500c8809870b969a25b5
ryantodd <ryantodd1176@gmail.com>
2021-05-04 12:30:18 +0300

    GetData

--- js/main.js
+++ js/main.js
@@ -5,24 +5,22 @@ let taskNameInput = form.elements.nameTask;
 let deskInput = form.elements.descTask;
 let dateTask = form.elements.dateTask;
 
+let tasks = [];
+
 form.addEventListener("submit", submitForm);
 function submitForm(event) {
     event.preventDefault();
-    //console.log(taskNameInput.value);
-    //console.log(deskInput = form.elements.descTask.value);
-    //console.log(dateTask = form.elements.dateTask.value);
 
     let task = {
         title: taskNameInput.value,
         description: deskInput.value,
         date: dateTask.value
     };
-    
-    console.log(task);
-    let tasks = localStorage.getItem("tasks");
-    if (!tasks) {
-        tasks = [];
-    }
-    tasks.push(task);
 
+    tasks = JSON.parse(localStorage.getItem(task));
+    if (!tasks) tasks = [];
+
+    localStorage.setItem("tasks", JSON.stringify(task));
+    tasks.push(task);
 }
+console.log(localStorage);
