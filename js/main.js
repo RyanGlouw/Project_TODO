aef00c31924b6b6becc83c5c8f540b6afa901eb6
ryantodd <ryantodd1176@gmail.com>
2021-05-13 17:35:39 +0300

    2PageDone

--- js/main.js
+++ js/main.js
@@ -5,40 +5,62 @@ let taskNameInput = form.elements.nameTask;
 let deskInput = form.elements.descTask;
 let dateTask = form.elements.dateTask;
 const addParticipantBtn = form.elements.addParticipant;
-const participantInputs = document.querySelector('.participant-inputs');
+const participantInputs = document.querySelector(".participant-inputs");
 let sumbitBtn = form.querySelector('[type="submit"]');
 
 const tasks = [];
 initLocalStorage();
 
 function initLocalStorage() {
-	if (localStorage.getItem('tasks') !== null) {
-		const tasksLocalStorage = JSON.parse(localStorage.getItem('tasks'));
-		if (Array.isArray(tasksLocalStorage)) {
-			tasks.push(...tasksLocalStorage);
-		}
-	};
+  if (localStorage.getItem("tasks") !== null) {
+    const tasksLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+    if (Array.isArray(tasksLocalStorage)) {
+      tasks.push(...tasksLocalStorage);
+    }
+  } else {
+    localStorage.setItem("tasks", JSON.stringify([]));
+  }
 }
 
 form.addEventListener("submit", submitForm);
 function submitForm(event) {
+  event.preventDefault();
 
-	event.preventDefault();
+  const participantInputs = Array.from(
+    form.querySelectorAll(".participant-input")
+  );
 
-	const participantInputs = Array.from(form.querySelectorAll('.participant-input'));
-	let arrValueParticipantInputs = participantInputs.map(item => item.value).filter(item => item.length !== 0);
+  const arrValueParticipantInputs = participantInputs
+    .map((item) => item.value)
+    .filter((item) => item.length !== 0);
+
+	const arrIdTasks = tasks.map(item => item.id);
+	const unicalId = generateUnicalId(arrIdTasks);
 
   const task = {
     title: taskNameInput.value,
     description: deskInput.value,
     date: dateTask.value,
-	participants: arrValueParticipantInputs
+    participants: arrValueParticipantInputs,
+		id: unicalId
   };
 
-	tasks.push(task);
-	console.log(tasks);
+  tasks.push(task);
+  console.log(tasks);
 
   localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function generateUnicalId(arrId) {
+  if (!Array.isArray(arrId)) return false;
+	if (arrId.length === 0) return 0;
+	const arrIdSort = arrId.slice().sort((a, b) => a - b);
+	for (let i = 0; i < arrIdSort.length; i++) {
+		if (arrIdSort[i] !== i) {
+			return i;
+		}
+	}
+	return arrIdSort.length;
 }
 
 // let tasksToArr = JSON.parse(localStorage.getItem("tasks"));
@@ -48,37 +70,41 @@ function submitForm(event) {
 // }
 
 function generateInputParticipant(domContainer) {
-	const InputParticipantContainer = document.createElement('div');
-	InputParticipantContainer.classList.add('participant-inputs__item');
+  const InputParticipantContainer = document.createElement("div");
+  InputParticipantContainer.classList.add("participant-inputs__item");
 
-	const inputText = document.createElement('input');
-	inputText.type = 'text';
-	inputText.placeholder = 'Введите имя участника';
-	inputText.classList.add('participant-input');
+  const inputText = document.createElement("input");
+  inputText.type = "text";
+  inputText.placeholder = "Введите имя участника";
+  inputText.classList.add("participant-input");
 
-	const inputButton = document.createElement('input');
-	inputButton.type = 'button';
-	inputButton.value = 'X';
+  const inputButton = document.createElement("input");
+  inputButton.type = "button";
+  inputButton.value = "X";
 
-	inputButton.addEventListener('click', (e) => {
-		InputParticipantContainer.remove();
-	});
+  inputButton.addEventListener("click", (e) => {
+    InputParticipantContainer.remove();
+  });
 
-	InputParticipantContainer.append(inputText);
-	InputParticipantContainer.append(inputButton);
+  InputParticipantContainer.append(inputText);
+  InputParticipantContainer.append(inputButton);
 
-	domContainer.append(InputParticipantContainer);
+  domContainer.append(InputParticipantContainer);
 }
 
-
-
-addParticipantBtn.addEventListener('click', (e) => {
-	generateInputParticipant(participantInputs);
+addParticipantBtn.addEventListener("click", (e) => {
+  generateInputParticipant(participantInputs);
 });
+
+document.getElementById("butt").addEventListener("click", message);
+
+function message(){
+document.getElementById("message").innerHTML = 'Задача успешно добавлена';
+}
 
 // let sumbitBtn = form.querySelector('[type="submit"]')
 // sumbitBtn.addEventListener('click', message)
 // function message(event){
-// 	console.log('Добавлено');
+// 	console.log('Задача успешно добавлена');
 // }
 // generateInputParticipant(participantInputs);
--- js/tasks.js
+++ js/tasks.js
@@ -1,108 +1,100 @@
-const list = document.querySelector('ul');
-const deleteTaskBtn = document.querySelector('.deleteTasks');
+const list = document.querySelector("ul");
+const deleteTaskBtn = document.querySelector(".deleteTasks");
 let tasksActive = [];
 
 function initLocalStorage() {
-    if (localStorage.getItem("tasks") !== null) {
-        const tasksToArr = JSON.parse(localStorage.getItem("tasks"));
-        if (Array.isArray(tasksToArr)) {
-            printTasks(tasksToArr, list);
-        }
+  if (localStorage.getItem("tasks") !== null) {
+    const tasksToArr = JSON.parse(localStorage.getItem("tasks"));
+    if (Array.isArray(tasksToArr)) {
+      printTasks(tasksToArr, list);
     }
+  } else {
+    localStorage.setItem("tasks", JSON.stringify([]));
+  }
 }
 
 function printTasks(tasks, container) {
-    for (let i = 0; i < tasks.length; i++) {
-        const taskObj = tasks[i];
-        const taskDom = generateTaskDom(taskObj, i);
-        container.append(taskDom);
-    }
+  for (let i = 0; i < tasks.length; i++) {
+    const taskObj = tasks[i];
+    const taskDom = generateTaskDom(taskObj);
+    container.append(taskDom);
+  }
 }
 
-function generateTaskDom(task, id) {
-    const domTask = document.createElement('div');
-    domTask.classList.add('task');
-    domTask.innerHTML = `
+function generateTaskDom(task) {
+  const domTask = document.createElement("div");
+  domTask.classList.add("task");
+  domTask.innerHTML = `
         <p class="task__title">${task.title}</p>
         <p class="task__description">${task.description}</p>
         <p class="task__date">${task.date}</p>
     `;
-    domTask.setAttribute('data-id', id);
+  domTask.setAttribute("data-id", task.id);
 
-    if (task.participants.length !== 0) {
+  if (task.participants.length !== 0) {
+    const taskParticipantsDom = document.createElement("div");
+    taskParticipantsDom.classList.add("task__participants");
 
-        const taskParticipantsDom = document.createElement('div');
-        taskParticipantsDom.classList.add('task__participants');
-        
-        for (const participant of task.participants) {
-            taskParticipantsDom.innerHTML += `<div class="task__participants-item">${participant}</div>`;
-        }
-        domTask.append(taskParticipantsDom);
+    for (const participant of task.participants) {
+      taskParticipantsDom.innerHTML += `<div class="task__participants-item">${participant}</div>`;
     }
+    domTask.append(taskParticipantsDom);
+  }
 
-    domTask.addEventListener('click', (e) => {
-        const dataId = Number(domTask.getAttribute('data-id'));
+  domTask.addEventListener("click", (e) => {
 
-        if (domTask.classList.contains('active')) {
-            const idFindRemoveId = tasksActive.find(item => item === dataId);
-            if (idFindRemoveId !== undefined) {
-                const idxRemoveId = tasksActive.findIndex(item => item === dataId);
-                if (idxRemoveId !== -1) {
-                    tasksActive.splice(idxRemoveId, 1);
-                    console.log(tasksActive);
-                }
-            }
-            domTask.classList.remove('active');
-        } else {
-            tasksActive.push(dataId);
-            domTask.classList.add('active');
-            const unicalIds = [...new Set([...tasksActive])];
-            tasksActive = unicalIds;
-            console.log(tasksActive);
+    if (domTask.classList.contains("active")) {
+      const idFindRemoveId = tasksActive.find((item) => item === task.id);
+      if (idFindRemoveId !== undefined) {
+        const idxRemoveId = tasksActive.findIndex((item) => item === task.id);
+        if (idxRemoveId !== -1) {
+          tasksActive.splice(idxRemoveId, 1);
         }
+      }
+      domTask.classList.remove("active");
+    } else {
+      tasksActive.push(task.id);
+      domTask.classList.add("active");
+      const unicalIds = [...new Set([...tasksActive])];
+      tasksActive = unicalIds;
+    }
+		
+  });
 
-    });
-
-    return domTask;
+  return domTask;
 }
 
-function deleteTasks(tasksActive, container) {
-
-    if (localStorage.getItem("tasks") !== null) {
-        const tasksToArr = JSON.parse(localStorage.getItem("tasks"));
-        if (Array.isArray(tasksToArr)) {
-            console.log(tasksActive, 'tasksActive');
-            console.log(tasksToArr, 'tasksToArr');
-            for (let i = 0; i < tasksActive.length; i++) {
-                const taskId = tasksActive[i];
-                if (tasksToArr[taskId] !== undefined) {
-                    console.log(i);
-                    tasksToArr.splice(taskId, 1);
-                    i--;
-                }
-            }
-            console.log(tasksToArr, 'tasksToArr delete');
-            // localStorage.setItem('tasks', JSON.stringify(tasksToArr));
-        }
+function deleteTasks(container) {
+  if (localStorage.getItem("tasks") !== null) {
+    const tasksToArr = JSON.parse(localStorage.getItem("tasks"));
+    if (Array.isArray(tasksToArr)) {
+      console.log(tasksActive, "tasksActive");
+      console.log(...tasksToArr, "tasksToArr");
+      for (let i = 0; i < tasksActive.length; i++) {
+        const taskId = tasksActive[i];
+				const idxTask = tasksToArr.findIndex(item => item.id === taskId);
+				if (idxTask !== -1) {
+					tasksToArr.splice(idxTask, 1);
+				}
+      }
+      console.log(tasksToArr, "tasksToArr delete");
+      localStorage.setItem('tasks', JSON.stringify(tasksToArr));
     }
+  }
 
-    const tasksActiveDom = tasksActive.map(item => {
-        return container.querySelector(`.task[data-id="${item}"]`);
-    });
+  const tasksActiveDom = tasksActive.map((item) => {
+    return container.querySelector(`.task[data-id="${item}"]`);
+  });
 
-    console.log(tasksActive, "tasksActive");
-    console.log(tasksActiveDom);
+  for (const taskActiveDom of tasksActiveDom) {
+    taskActiveDom.remove();
+  }
 
-    for (const taskActiveDom of tasksActiveDom) {
-        taskActiveDom.remove();
-    }
-
-    tasksActive = [];
-    console.log(tasksActive, "tasksActive []");
+  tasksActive = [];
 }
 
 initLocalStorage();
 
-deleteTaskBtn.addEventListener('click', (e) => {
-    deleteTasks(tasksActive, list);
+deleteTaskBtn.addEventListener("click", (e) => {
+  deleteTasks(list);
 });
